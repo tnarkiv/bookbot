202891ae4685ab734135c2a654b2b41706897026
tnarkiv <vikrant2099ad@gmail.com>
2025-02-22 04:44:11 +0000

    Fixed as per linting suggestions

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-from stats import count_number_of_words
-import sys
+""" for sys.argv variables """
 
-def count_characters(text):    
+import sys
+from stats import count_number_of_words
+
+
+def count_characters(text):
+    """counts the number of characters in a given text string
+
+    Args:
+        text (_type_): string
+
+    Returns:
+        _type_: integer
+    """
     text = text.lower()
     character_dictionary = {}
     for character in text:
-        if(character in character_dictionary):
+        if character in character_dictionary:
             character_dictionary[character] += 1
         else:
             character_dictionary[character] = 1
     return character_dictionary
 
+
 def main():
+    """main function"""
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
@@ -20,14 +33,15 @@ def main():
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {path}...")
     print("----------- Word Count ----------")
-    with open(path) as f:
-        text = f.read()    
+    with open(path, encoding="utf-8") as f:
+        text = f.read()
     number_of_words = count_number_of_words(text)
     print(f"Found {number_of_words} total words")
     print("--------- Character Count -------")
     character_dictionary = count_characters(text)
     for key, value in character_dictionary.items():
-        if(key >= 'a' and key <= 'z'):
+        if key >= "a" and key <= "z":
             print(f"{key}: {value}")
+
 
 main()
